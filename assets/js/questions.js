c9502b44ff5403ae2cf5f3def91bc1f91e6fdf1b
ARcKP98 <pkartik4386@outlook.com>
2023-01-13 18:12:56 +0000

    Add easy level questions.

---
+++ assets/js/questions.js
@@ -0,0 +1,82 @@
+const easyQuestions = [
+    {
+        question : 'Which movie started the Marvel Cinematic universe?',
+        1 : 'The Avengers',
+        2 : 'Captain America: The First Avenger', 
+        3 : 'Thor',
+        4 : 'Iron Man', 
+        correct : 'Iron Man'
+    },
+    {
+        question : "What is the name of Thor's hammer?",
+        1 : 'Mjolnir',
+        2 : 'Storm breaker', 
+        3 : 'Jonathan',
+        4 : ' Gugnir', 
+        correct : 'Mjolnir'
+    },
+    {
+        question : 'What does the D in SHIELD stand for?',
+        1 : 'Defense',
+        2 : 'Division', 
+        3 : 'Damage',
+        4 : 'Department', 
+        correct : 'Division'
+    },
+    {
+        question : 'How many Avengers were there originally in the MCU?',
+        1 : '6',
+        2 : '7', 
+        3 : '8',
+        4 : '9', 
+        correct : '6'
+    },
+    {
+        question : 'Who is not considered an Avenger in the MCU?',
+        1 : 'SpiderMan',
+        2 : 'Black Widow', 
+        3 : 'Black Panther',
+        4 : 'Hulk', 
+        correct : 'Black Panther'
+    },
+    {
+        question : 'Who is related to Loki in the MCU',
+        1 : 'Thor',
+        2 : 'Bucky Barnes', 
+        3 : 'Sylvie',
+        4 : 'Bruce Banner', 
+        correct : 'Thor'
+    },
+    {
+        question : 'What is the name of the alien force that attacked New York in The Avengers?',
+        1 : 'The Chitauri',
+        2 : 'Black Order', 
+        3 : 'Flag Smashers',
+        4 : 'Hydra', 
+        correct : 'The Chitauri'
+    },
+    {
+        question : 'Which movie started the Marvel Cinematic universe?',
+        1 : 'The Avengers',
+        2 : 'Captain America: The First Avenger', 
+        3 : 'Thor',
+        4 : 'Iron Man', 
+        correct : 'Iron Man'
+    },
+    {
+        question : "Who is Peter Quill's father",
+        1 : 'Yondu',
+        2 : 'Ego', 
+        3 : 'Kurt',
+        4 : 'Odin', 
+        correct : 'Ego'
+    },
+    {
+        question : 'Who is the main villain in Thor:The Dark World?',
+        1 : 'Loki',
+        2 : 'Captain America: The First Avenger', 
+        3 : 'Gorr',
+        4 : 'Eitri', 
+        correct : 'Malekith'
+    },
+]
