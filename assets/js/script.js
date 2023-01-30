1c32634994b52b5b329264b0f0bfb2048eabd75d
ARcKP98 <pkartik4386@outlook.com>
2023-01-30 20:55:55 +0000

    Add comments to JS files.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-//select all the relevant variables 
+//select all the relevant variables to be used. 
 const welcomeBox = document.getElementById("game-box-welcome");
 const levelButton1 = document.getElementById("level-easy-btn");
 const levelButton2 = document.getElementById("level-medium-btn");
@@ -22,16 +22,16 @@ const finalScore = document.getElementById("score");
 const retryButton = document.getElementById("play-again");
 const image = document.getElementById("img-result");
 
-let currentQuestion = {};
-let acceptingAnswers = false;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-let correctAns = 0;
-let incorrectAns = 0;
-let correctQ = 1;
-let correctBonus = 10;
-let maxQuestions = 10;
+let currentQuestion = {}; //empty object which will be updated during the game with the question. Display question.
+let acceptingAnswers = false; //helps with the time lag between questions.
+let score = 0; //set the score to 0.
+let questionCounter = 0; //set the question counter to 0.
+let availableQuestions = []; //An empty array which will hold different questions depending on the game level.
+let correctAns = 0; //Set the correct answer score tracker to 0.
+let incorrectAns = 0; // Set the incorrect answer score tracker to 0.
+let correctQ = 1; //To increment/decrement the corret and incorrect score tracker by 1. 
+let correctBonus = 10; //To increment/decrement points depending on the answer by 10.
+let maxQuestions = 10; //Number of questions displayed during each level. Used this for progressbar/to determine if the game is over.
 
 
 //Toggling betwen the rules section and the home screen. 
@@ -53,7 +53,9 @@ retryButton.addEventListener('click', () => {
     window.location.reload();
 });
 
-//Easy level game
+//Easy level game. Upon pressing the easy level button, the function startGameEasy is being run. 
+//The score and questionCounter are set to 0 and will be incremented as the game progresses. The available question
+// array contains all the easy array elements(questions) from questions.js.
 levelButton1.addEventListener('click',
     function startGameEasy() {
         questionCounter = 0;
@@ -66,7 +68,9 @@ levelButton1.addEventListener('click',
     }
 );
 
-//Medium level game
+//Medium level game. Upon pressing the easy level button, the function startGameMedium is being run. 
+//The score and questionCounter are set to 0 and will be incremented as the game progresses. The available question
+// array contains all the medium array elements(questions) from questions.js.
 levelButton2.addEventListener('click',
     function startGameMedium() {
         questionCounter = 0;
@@ -78,7 +82,9 @@ levelButton2.addEventListener('click',
         showNextQuestion();
     }
 );
-//Hard level game
+//Hard level game. Upon pressing the easy level button, the function startGameExcelsior is being run. 
+//The score and questionCounter are set to 0 and will be incremented as the game progresses. The available question
+// array contains all the excelsior array elements(questions) from questions.js.
 levelButton3.addEventListener('click',
     function startGameExcelsior() {
         questionCounter = 0;
@@ -91,6 +97,13 @@ levelButton3.addEventListener('click',
     }
 );
 
+//This function displays questions and the relevant answer options to the user. 
+//It checks whether a question should be displayed or not by checking how many questions are still available.
+//If there are no questions left to display from the list, the screen switches to the result section and shows the 
+//score along with a picture based on the score range.
+//If there are question available, the function uses the questions from the availableQuestions array based on the
+//game type and displays the question and answer property from the list to the HTML. The code also updates the progress
+//bar as the questionCounter increases. 
 function showNextQuestion() {
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
         mainGame.classList.add("hide");
@@ -124,6 +137,11 @@ function showNextQuestion() {
 
 }
 
+//Checks which answer was selected by the user and compares it to the correct answer.
+//It shows the user the answer they chose and the correct answer if they chose incorrect by highlighting
+//the answers. It removes the hover functionality so that the user cannot choose another option. 
+//Based on the answer, the score is incremented/decremented by the value(s) defined above.
+//The code also adds a time buffer of 2.5 seconds to move to the next question.
 function checkAnswer() {
     answerBtns.forEach(answer => {
         answer.addEventListener('click', e => {
@@ -181,7 +199,7 @@ function checkAnswer() {
         incorrect_score_keep.innerText = incorrectAns;
     };
 }
-
+// Reset the correct/incorrect answer highlights from previous question and enables the hover effect again. 
 function resetQstate() {
     answerOption1.classList.remove('correct-ans');
     answerOption2.classList.remove('correct-ans');
